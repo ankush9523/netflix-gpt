b0445bbd201f640c813e4daccdc0372299c38466
Ankush Sah <ankushkhizersarai@gmail.com>
2024-01-10 22:40:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+<> 
+  <div className='text-3xl font-bold  text-green-600'> Namastey React! let's develop the netlix-gpt app</div>
+    </>
+  );
+}
+
+export default App;
